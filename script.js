41f7f3fdb0bbe1d2522d7eadaa7f642d8992f949
Dipa-Hole <125375562+Dipa-Hole@users.noreply.github.com>
2024-12-02 16:52:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// // Example: Alert for Add to Cart
+// const buttons = document.querySelectorAll('button');
+// buttons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         alert('Added to cart!');
+//     });
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const hamburgerMenu = document.querySelector(".hamburger-menu");
+//     const filterSidebar = document.querySelector(".filter-sidebar");
+
+//     // Toggle the filter sidebar on hamburger menu click
+//     hamburgerMenu.addEventListener("click", function () {
+//         filterSidebar.classList.toggle("active");
+//     });
+// });
+
